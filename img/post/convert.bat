632abb3f6459d6063fd03ddd88427e3afc88301d
zhizunjiege <2452816044@qq.com>
2020-06-27 22:41:56 +0800

    Site updated: 2020-06-27 22:41:54

--- img/post/convert.bat
+++ img/post/convert.bat
@@ -1,3 +1,3 @@
 @echo off
-for %%i in (*.png) do start E:\ffmpeg\bin\ffmpeg -i "%%i" "%%~dpni.webp"
+for %%i in (*.gif) do start E:\ffmpeg\bin\ffmpeg -i "%%i" "%%~dpni.webp"
 REM for %%i in (*.jpeg) do start E:\ffmpeg\bin\ffmpeg -i "%%i" "%%~dpni.webp"
