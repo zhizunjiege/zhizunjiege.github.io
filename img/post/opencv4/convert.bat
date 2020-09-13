e3f7be22ab858f197d74722e4439c3d518910a55
zhizunjiege <2452816044@qq.com>
2020-09-13 23:08:46 +0800

    Site updated: 2020-09-13 23:08:45

--- img/post/opencv4/convert.bat
+++ img/post/opencv4/convert.bat
@@ -1,3 +1,3 @@
 @echo off
-for %%i in (*.webp) do start E:\ffmpeg\bin\ffmpeg -i "%%i" "%%~dpni.png"
+for %%i in (*.png) do start E:\ffmpeg\bin\ffmpeg -i "%%i" "%%~dpni.webp"
 REM for %%i in (*.jpeg) do start E:\ffmpeg\bin\ffmpeg -i "%%i" "%%~dpni.webp"
